4734ba416d47dfb2a4123588307eaddda71c5f24
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-08-23 10:26:31 +0700

    feat: menambahkan color schame vs codium

--- home.nix
+++ home.nix
@@ -1,10 +1,8 @@
 { config, pkgs, ... }:
 let
-  # Sesuaikan path ini dengan lokasi folder dotfiles-mu yang sebenarnya
   dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
   create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
 
-  # Daftar folder yang mau di-symlink ke ~/.config/
   configs = {
     waybar = "waybar";
     rofi = "rofi";
@@ -16,9 +14,8 @@ in
 {
   home.username = "enzwich";
   home.homeDirectory = "/home/enzwich";
-  home.stateVersion = "26.05"; # Sesuaikan dengan versimu
+  home.stateVersion = "26.05";
 
-  # Ini yang bertugas membuat symlink dotfiles
   xdg.configFile = builtins.mapAttrs
     (name: subpath: {
       source = create_symlink "${dotfiles}/${subpath}";
@@ -26,7 +23,6 @@ in
     })
     configs;
 
-  # Tema catpucin
   home.pointerCursor = {
     enable = true;
     gtk.enable = true;
@@ -36,7 +32,6 @@ in
     size = 24;
   };
 
-  # Mengatur Tema Aplikasi GTK (termasuk Thunar) dan Ikon
   gtk = {
     enable = true;
     theme = {
@@ -58,5 +53,21 @@ in
       package = pkgs.catppuccin-cursors.mochaMauve;
     };
   };
+
+  # VSCODIUM DI SINI
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium; 
+    
+    extensions = with pkgs.vscode-extensions; [
+      catppuccin.catppuccin-vsc
+    ];
+
+    userSettings = {
+      "workbench.colorTheme" = "Catppuccin Mocha"; 
+      "catppuccin.accentColor" = "mauve"; 
+    };
+  };
+
   home.file.".zshrc".source = create_symlink "${dotfiles}/.zshrc";
 }
