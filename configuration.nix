a89490e29c22c8ac644e3c75a09afd1775ed003e
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-08-23 17:44:53 +0700

    feat: menambahkan github cli

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,7 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     tree
     git
+    gh
     foot
     gcc
     lua-language-server
