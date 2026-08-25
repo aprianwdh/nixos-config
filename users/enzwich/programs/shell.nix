902cbf536eb2ac204af53ab41e1a7907c4d27971
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-08-26 00:32:33 +0700

    feat: add foot as default terminal

--- users/enzwich/programs/shell.nix
+++ users/enzwich/programs/shell.nix
@@ -3,5 +3,13 @@
 {
   home.sessionVariables = {
     EDITOR = "nvim";
+    TERMINAL = "foot";
+  };
+
+  xdg.terminal-exec = {
+    enable = true;
+    settings = {
+      default = [ "foot.desktop" ];
+    };
   };
 }
