156763d4f89c3e7409400324a59775686c08460d
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-08-28 16:27:26 +0700

    feat: remove pinned from flake.nix

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
           {
             # "pinned" = pakai revisi nixpkgs persis seperti yang dipakai nix-cachyos-kernel,
             # ini yang menjamin kamu dapat binary cache (tidak compile dari source).
-            nixpkgs.overlays = [ nix-cachyos-kernel.overlays ];
+            nixpkgs.overlays = [ nix-cachyos-kernel.overlays.default ];
 
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
