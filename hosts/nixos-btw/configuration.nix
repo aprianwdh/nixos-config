d169e1182aa8d36a051f88a415853667fa386420
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-08-28 16:07:43 +0700

    feat: readd cachyos custom kernel

--- hosts/nixos-btw/configuration.nix
+++ hosts/nixos-btw/configuration.nix
@@ -16,7 +16,7 @@
   ];
 
   # Boot
-  #boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
