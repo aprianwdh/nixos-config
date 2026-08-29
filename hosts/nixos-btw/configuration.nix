3008139a5c99658eca0c140bb011973a4352c7b3
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-08-29 08:57:20 +0700

    feat: enable cachyos kernel

--- hosts/nixos-btw/configuration.nix
+++ hosts/nixos-btw/configuration.nix
@@ -16,7 +16,7 @@
   ];
 
   # Boot
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-x86_64-v3;
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
