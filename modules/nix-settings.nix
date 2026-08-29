86e0ff91b2dd17f402df96f096f9d9c561609e43
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-08-29 08:52:59 +0700

    feat: enable cachyos kernel

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -12,10 +12,12 @@
       "enzwich"
     ];
     extra-substituters = [
+      "https://cache.xinux.uz"
       "https://hyprland.cachix.org"
       "https://attic.xuyh0120.win/lantian"
     ];
     extra-trusted-public-keys = [
+      "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
     ];
