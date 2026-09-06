15a07c269fe1385a4388ee59d73e80f41cf41586
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-09-06 09:34:53 +0700

    feat: fix uwsm niri

--- hosts/nixos-btw/configuration.nix
+++ hosts/nixos-btw/configuration.nix
@@ -34,13 +34,9 @@ in
     settings = {
       default_session = {
         user = "greeter";
-        command = ''
-          ${lib.getExe pkgs.tuigreet} \
-            --time \
-            --asterisks \
-            --user-menu \
-            --sessions ${sessionsDir}/wayland-sessions
-        '';
+        command = "
+          ${lib.getExe pkgs.tuigreet} --time --asterisks --user-menu --sessions ${sessionsDir}/wayland-sessions
+        ";
       };
     };
   };
