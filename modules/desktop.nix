b72beefad5b085f250861484a85289fedb4c3ae8
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-09-06 08:07:35 +0700

    feat: fix uwsm niri

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -26,7 +26,7 @@
     waylandCompositors.niri = {
       prettyName = "Niri";
       comment = "Niri compositors managed by UWSM";
-      binPath = "${pkgs.niri}/bin/niri";
+      binPath = "/run/current-system/sw/bin/niri";
     };
   };
   # file manager & dconf
