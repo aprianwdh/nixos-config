abaaf03a7a333e9b58a22a8006f1b11eeea71ef9
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-09-14 20:22:23 +0700

    feat: add virtual machine

--- hosts/nixos-btw/configuration.nix
+++ hosts/nixos-btw/configuration.nix
@@ -56,6 +56,7 @@ in
       "networkmanager"
       "wheel"
       "video"
+      "libvirtd"
     ];
     packages = with pkgs; [ ];
   };
---
+++ modules/virtualisation.nix
@@ -0,0 +1,2 @@
+virtualisation.libvirtd.enable = true;
+programs.virt-manager.enable = true;
