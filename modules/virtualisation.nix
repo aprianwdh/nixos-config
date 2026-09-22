dbbedf64b17a1f5d90fd9baf191214d86caed666
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-09-22 17:36:14 +0700

    feat: add backend podman for 9router

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ...}:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   # virtualisation.libvirtd.enable = true;
@@ -10,10 +15,11 @@
       dockerCompat = true;
     };
     oci-containers.backend = "podman";
-    
+
     oci-containers.containers."9router" = {
       image = "decolua/9router:latest";
       ports = [ "11434:11434" ];
     };
+  };
   programs.virt-manager.enable = true;
 }
